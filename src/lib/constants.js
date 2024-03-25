258de8745cfa0f1209b9524d3ec14df6b9184698
Rayhan Arkan <rayhanarkan30@gmail.com>
2024-03-25 17:23:13 +0700

    fix env

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,5 +1,5 @@
 export const ENV = {
-    NEXT_PUBLIC_WEBSITE_URL: process.env.NEXT_PUBLIC_WEBSITE_URL || 'http://localhost:3000',
+    NEXT_PUBLIC_WEBSITE_URL: process.env.NEXT_PUBLIC_WEBSITE_URL,
     NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || '',
     NODE_ENV: process.env.NODE_ENV || 'production'
   }
