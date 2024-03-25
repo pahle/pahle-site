fe450c00b70cc5be8e70c5ed9a6d25626aa78865
Rayhan Arkan <rayhanarkan30@gmail.com>
2024-03-25 19:44:05 +0700

    fix footer flex

--- src/app/uses/page.jsx
+++ src/app/uses/page.jsx
@@ -42,25 +42,6 @@ export default function Uses() {
             single time, even under the incredibly heavy loads I put it through
             with our various launch simulations.
           </Tool>
-          {/* <Tool title="Apple Pro Display XDR (Standard Glass)">
-            The only display on the market if you want something HiDPI and
-            bigger than 27”. When you’re working at planetary scale, every pixel
-            you can get counts.
-          </Tool>
-          <Tool title="IBM Model M SSK Industrial Keyboard">
-            They don’t make keyboards the way they used to. I buy these any time
-            I see them go up for sale and keep them in storage in case I need
-            parts or need to retire my main.
-          </Tool>
-          <Tool title="Apple Magic Trackpad">
-            Something about all the gestures makes me feel like a wizard with
-            special powers. I really like feeling like a wizard with special
-            powers.
-          </Tool>
-          <Tool title="Herman Miller Aeron Chair">
-            If I’m going to slouch in the worst ergonomic position imaginable
-            all day, I might as well do it in an expensive chair.
-          </Tool> */}
         </ToolsSection>
         <ToolsSection title="Development tools">
           <Tool title="Visual Studio Code">
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,12 +22,12 @@ export function Footer() {
         <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
           <ContainerInner>
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
-              <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
+              <div className="flex flex-wrap justify-start gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200 flex-1">
                 <NavLink href="/about">About</NavLink>
                 <NavLink href="/projects">Projects</NavLink>
                 <NavLink href="/uses">Uses</NavLink>
               </div>
-              <div className="text-sm font-medium text-zinc-800 dark:text-zinc-200">
+              <div className="text-sm font-medium text-zinc-800 dark:text-zinc-200 shrink-0">
                 Special thanks to :{' '}
                 <Sparkles>
                   <Link
@@ -38,8 +38,8 @@ export function Footer() {
                   </Link>
                 </Sparkles>
               </div>
-              <p className="text-sm text-zinc-400 dark:text-zinc-500">
-                {new Date().getFullYear()} Pahle.
+              <p className="flex justify-end text-sm text-zinc-400 dark:text-zinc-500 flex-1">
+                &copy; {new Date().getFullYear()} Pahle. All rights reserved.
               </p>
             </div>
           </ContainerInner>
